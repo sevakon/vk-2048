5769c45ef44ff28de1c26d3b861e334424f92190
Vsevolod Konyakhin <vsevolodkonyakhin@mail.ru>
2019-05-22 00:29:50 +0300

    Initial Commit

---
+++ vk-internship-2048/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vk-internship-2048
+//
+//  Created by Владимир Коняхин on 22/05/2019.
+//  Copyright © 2019 Vsevolod Konyakhin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
